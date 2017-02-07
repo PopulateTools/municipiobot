a03537364363a8e5b7444502213b7e215b605abe
Martín González <m@martingonzalez.net>
2017-02-07 16:17:35 +0100

    Clean the script, add the dataset

--- data/municipality_budgets/get_data.r
+++ data/municipality_budgets/get_data.r
@@ -3,20 +3,22 @@ library(jsonlite)
 library(dplyr)
 
 # Get data and budget categories from the TBI API
-url_data <- "https://tbi.populate.tools/gobierto/datasets/ds-presupuestos-municipales-partida.json?sort_desc_by=date&with_metadata=true&filter_by_municipality_id=08019&filter_by_year=2016"
-get_data <- GET(url=url_data, add_headers(Authorization=paste("Bearer", Sys.getenv("POPULATE_DATA_TOKEN"), sep=" "), Origin="http://madrid.gobierto.dev"))
-data <- fromJSON(content(get_data, type="text"))
+url_budget <- "https://tbi.populate.tools/gobierto/datasets/ds-presupuestos-municipales-partida.json?sort_desc_by=date&with_metadata=true&filter_by_municipality_id=08019&filter_by_year=2016"
+get_budget <- GET(url=url_budget, add_headers(Authorization=paste("Bearer", Sys.getenv("POPULATE_DATA_TOKEN"), sep=" "), Origin="http://madrid.gobierto.dev"))
+budget <- fromJSON(content(get_budget, type="text"))
+budget <- budget$data
+budget <- filter(budget, is.na(functional_code))
 
 url_cat <- "https://tbi.populate.tools/gobierto/collections/c-categorias-presupuestos-municipales/items.json"
 get_cat <- GET(url=url_cat, add_headers(Authorization=paste("Bearer", Sys.getenv("POPULATE_DATA_TOKEN"), sep=" "), Origin="http://madrid.gobierto.dev"))
 cat <- fromJSON(content(get_cat, type="text"))
 
 # Join
-df <- data$data
-df <- filter(df, is.na(functional_code))
+df <- merge(budget, cat, by=c("area","kind", "code"))
+df <- filter(df, area == 'functional') %>%
+  select(municipality_id, value_per_inhabitant, name)
 
-merged <- merge(df, cat, by=c("area","kind", "code"))
-merged <- filter(merged, area == 'functional')
+df$dataset <- "Gasto por habitante" 
 
 # Write the CSV
-#write.csv(merged, file = "output/data.csv")
+write.csv(df, file = "output/data.csv", row.names = FALSE)
