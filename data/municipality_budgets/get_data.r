b1bc551f87d885038f0885afd1fe95b077f25cea
Martín González <m@martingonzalez.net>
2017-02-07 15:51:31 +0100

    The script works now

--- data/municipality_budgets/get_data.r
+++ data/municipality_budgets/get_data.r
@@ -1,13 +1,22 @@
 library(httr)
 library(jsonlite)
+library(dplyr)
 
-# Get data from the TBI API
-url <- "https://tbi.populate.tools/gobierto/datasets/ds-presupuestos-municipales-partida.json?sort_desc_by=date&with_metadata=true&filter_by_municipality_id=39075"
-get_data <- GET(url=url, add_headers(Authorization=paste("Bearer", Sys.getenv("POPULATE_DATA_TOKEN"), sep=" "), Origin="http://madrid.gobierto.dev"))
+# Get data and budget categories from the TBI API
+url_data <- "https://tbi.populate.tools/gobierto/datasets/ds-presupuestos-municipales-partida.json?sort_desc_by=date&with_metadata=true&filter_by_municipality_id=08019&filter_by_year=2016"
+get_data <- GET(url=url_data, add_headers(Authorization=paste("Bearer", Sys.getenv("POPULATE_DATA_TOKEN"), sep=" "), Origin="http://madrid.gobierto.dev"))
 data <- fromJSON(content(get_data, type="text"))
+
+url_cat <- "https://tbi.populate.tools/gobierto/collections/c-categorias-presupuestos-municipales/items.json"
+get_cat <- GET(url=url_cat, add_headers(Authorization=paste("Bearer", Sys.getenv("POPULATE_DATA_TOKEN"), sep=" "), Origin="http://madrid.gobierto.dev"))
+cat <- fromJSON(content(get_cat, type="text"))
 
 # Join
 df <- data$data
+df <- filter(df, is.na(functional_code))
+
+merged <- merge(df, cat, by=c("area","kind", "code"))
+merged <- filter(merged, area == 'functional')
 
 # Write the CSV
-#write.csv(df, file = "data.csv")
+#write.csv(merged, file = "output/data.csv")
